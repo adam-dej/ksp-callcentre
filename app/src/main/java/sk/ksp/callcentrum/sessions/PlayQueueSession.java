e786f6f6880af8e8f21a37f0b96bfb8d553db5ae
Adam <dejko.a@gmail.com>
2014-11-18 22:45:42 +0100

    Implement basic socket connection

--- app/src/main/java/sk/ksp/callcentrum/sessions/PlayQueueSession.java
+++ app/src/main/java/sk/ksp/callcentrum/sessions/PlayQueueSession.java
@@ -4,6 +4,11 @@ import android.content.res.Resources;
 import android.os.Handler;
 import android.util.Log;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 import sk.ksp.callcentrum.CallSessionManager;
 import sk.ksp.callcentrum.DataStorage;
 import sk.ksp.callcentrum.R;
@@ -12,7 +17,59 @@ public class PlayQueueSession extends CallSessionManager {
 
     private String phoneNumber;
 
-    public PlayQueueSession(Handler uiHandler, Resources resources, String phoneNumber) {
+    private class TimerUpdateRunnable implements Runnable {
+
+        private boolean stop = false;
+        private int seconds = 0;
+
+        @Override
+        public void run() {
+            while (!stop) {
+                try {
+                    Thread.sleep(1000);
+                    uiHandler.obtainMessage(MESSAGE_UPDATE_TIME, String.format("%02d:%02d", seconds / 60, seconds % 60)).sendToTarget();
+                    seconds++;
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        public void stopTimer() {
+            stop = true;
+        }
+    }
+
+    private TimerUpdateRunnable timerUpdateRunnable;
+
+    private class ServerCommThread implements Runnable {
+
+        private Socket commSocket;
+
+        @Override
+        public void run() {
+            uiHandler.obtainMessage(MESSAGE_SHOW_NUMBER, phoneNumber).sendToTarget();
+            uiHandler.obtainMessage(MESSAGE_SHOW_MESSAGEBAR, resources.getString(R.string.ksp_dialing)).sendToTarget();
+            try {
+                InetAddress serverAddr = InetAddress.getByName(DataStorage.getStorage().getServerAddress());
+                commSocket = new Socket(serverAddr, Integer.decode(DataStorage.getStorage().getServerPort()));
+                uiHandler.obtainMessage(MESSAGE_HIDE_MESSAGEBAR).sendToTarget();
+                timerUpdateRunnable = new TimerUpdateRunnable();
+                new Thread(timerUpdateRunnable).start();
+            } catch (UnknownHostException e) {
+                Log.e("PlayQueueSession", e.toString());
+                killCallWithMessage(resources.getString(R.string.ksp_no_signal));
+            } catch (IOException e) {
+                Log.e("PlayQueueSession", e.toString());
+                killCallWithMessage(resources.getString(R.string.ksp_no_signal));
+            }
+        }
+
+    }
+
+    private ServerCommThread serverCommThread;
+
+    public PlayQueueSession(final Handler uiHandler, final Resources resources, String phoneNumber) {
         super(uiHandler, resources);
         if (phoneNumber == null) {
             Log.wtf("PlayQueueSession", "phoneNumber is null!");
@@ -23,7 +80,21 @@ public class PlayQueueSession extends CallSessionManager {
             Log.wtf("PlayQueueSession", "getServerAddress() is null!");
             displayInternalError("2");
         } else {
-            displayInternalError("47");
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        uiHandler.obtainMessage(MESSAGE_SHOW_PROVIDER_INFO,
+                                resources.getString(R.string.ksp_network)).sendToTarget();
+                        Thread.sleep(1000);
+                        uiHandler.obtainMessage(MESSAGE_HIDE_PROVIDER_INFO).sendToTarget();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+            serverCommThread = new ServerCommThread();
+            new Thread(serverCommThread).start();
         }
     }
 
@@ -39,6 +110,21 @@ public class PlayQueueSession extends CallSessionManager {
                     Thread.sleep(6000);
                     uiHandler.obtainMessage(MESSAGE_HIDE_PROVIDER_INFO).sendToTarget();
                     Thread.sleep(600);
+                    uiHandler.obtainMessage(MESSAGE_DIE).sendToTarget();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    private void killCallWithMessage(final String message) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    uiHandler.obtainMessage(MESSAGE_SHOW_MESSAGEBAR, message).sendToTarget();
+                    Thread.sleep(3000);
                     uiHandler.obtainMessage(MESSAGE_DIE).sendToTarget();
                 } catch (InterruptedException e) {
                     e.printStackTrace();
