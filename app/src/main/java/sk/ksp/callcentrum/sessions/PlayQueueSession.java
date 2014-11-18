7a12be53a821d853f4469e297342d1a8d57fb2d4
Adam <dejko.a@gmail.com>
2014-11-18 23:20:28 +0100

    Implement basic server communication

--- app/src/main/java/sk/ksp/callcentrum/sessions/PlayQueueSession.java
+++ app/src/main/java/sk/ksp/callcentrum/sessions/PlayQueueSession.java
@@ -4,11 +4,16 @@ import android.content.res.Resources;
 import android.os.Handler;
 import android.util.Log;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.net.SocketException;
 import java.net.UnknownHostException;
 
+import sk.ksp.callcentrum.BuildConfig;
 import sk.ksp.callcentrum.CallSessionManager;
 import sk.ksp.callcentrum.DataStorage;
 import sk.ksp.callcentrum.R;
@@ -16,6 +21,7 @@ import sk.ksp.callcentrum.R;
 public class PlayQueueSession extends CallSessionManager {
 
     private String phoneNumber;
+    private boolean dialerOpen;
 
     private class TimerUpdateRunnable implements Runnable {
 
@@ -45,6 +51,56 @@ public class PlayQueueSession extends CallSessionManager {
     private class ServerCommThread implements Runnable {
 
         private Socket commSocket;
+        private OutputStreamWriter out;
+        private BufferedReader  in;
+
+        private int readerState = 0;
+
+        private static final int STATE_UNINITIALIZED = 0;
+        private static final int STATE_NORMAL = 1;
+        private static final int STATE_DEAD = 2;
+
+        private void serverWrite(String line) throws IOException{
+            if (BuildConfig.DEBUG) {
+                Log.d("PlayQueueSession", "-> " + line);
+            }
+            out.write(line + '\n');
+            out.flush();
+        }
+
+        private String serverRead() throws IOException {
+            String line = in.readLine();
+            if (BuildConfig.DEBUG) {
+                Log.d("PlayQueueSession", line == null ? "<- (NULL)" : "<- " + line);
+            }
+            return line;
+        }
+
+        private void handleCommFailure(Exception e) {
+            if (BuildConfig.DEBUG) {
+                Log.e("PlayQueueSession", e.toString());
+            }
+            readerState = STATE_DEAD;
+            killCallWithMessage(resources.getString(R.string.ksp_call_terminated));
+            timerUpdateRunnable.stopTimer();
+        }
+
+        public void sendButtonPress(char button) {
+            if (readerState == STATE_NORMAL) {
+                try {
+                    serverWrite("button " + button);
+                } catch (IOException e) {
+                    handleCommFailure(e);
+                }
+            }
+        }
+
+        public void killComm() {
+            try {
+                commSocket.close();
+            } catch (IOException e) {
+            }
+        }
 
         @Override
         public void run() {
@@ -56,12 +112,47 @@ public class PlayQueueSession extends CallSessionManager {
                 uiHandler.obtainMessage(MESSAGE_HIDE_MESSAGEBAR).sendToTarget();
                 timerUpdateRunnable = new TimerUpdateRunnable();
                 new Thread(timerUpdateRunnable).start();
+
+                out = new OutputStreamWriter(commSocket.getOutputStream());
+                in = new BufferedReader(new InputStreamReader(commSocket.getInputStream()));
+
+                serverWrite("druzinka " + BuildConfig.druzinkaName + " " + phoneNumber);
+
+                while (true) {
+                    String line = serverRead();
+                    if (line == null) {
+                        readerState = STATE_DEAD;
+                        killCallWithMessage(resources.getString(R.string.ksp_call_terminated));
+                        timerUpdateRunnable.stopTimer();
+                        break;
+                    }
+
+                    switch (readerState) {
+                        case STATE_UNINITIALIZED:
+                            if ("start".equals(line)) {
+                                readerState = STATE_NORMAL;
+                            }
+                            break;
+                        case STATE_NORMAL:
+
+                            break;
+                    }
+
+                }
+
             } catch (UnknownHostException e) {
                 Log.e("PlayQueueSession", e.toString());
                 killCallWithMessage(resources.getString(R.string.ksp_no_signal));
             } catch (IOException e) {
-                Log.e("PlayQueueSession", e.toString());
-                killCallWithMessage(resources.getString(R.string.ksp_no_signal));
+                if (e instanceof SocketException) {
+                    // Proper call termination
+                    readerState = STATE_DEAD;
+                    killCallWithMessage(resources.getString(R.string.ksp_call_terminated));
+                    timerUpdateRunnable.stopTimer();
+                } else {
+                    Log.e("PlayQueueSession", e.toString());
+                    killCallWithMessage(resources.getString(R.string.ksp_no_signal));
+                }
             }
         }
 
@@ -135,11 +226,19 @@ public class PlayQueueSession extends CallSessionManager {
 
     @Override
     public void onButtonClick(int button) {
-
+        switch (button) {
+            case R.id.dialpadButton:
+                uiHandler.obtainMessage(dialerOpen ? MESSAGE_HIDE_DIALPAD : MESSAGE_SHOW_DIALPAD).sendToTarget();
+                dialerOpen = !dialerOpen;
+                break;
+            case R.id.endButton:
+                serverCommThread.killComm();
+                break;
+        }
     }
 
     @Override
     public void onDialerClick(char dialerButton) {
-
+        serverCommThread.sendButtonPress(dialerButton);
     }
 }
